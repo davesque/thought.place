be4a5938906b986e920060c09a1262226fb18e90
David Sanders <davesque@gmail.com>
2017-06-22 22:29:00 -0600

    Fix python 3 error

--- thoughtplace/settings.py
+++ thoughtplace/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+from urllib.parse import urlparse
 import os
 import sys
-import urlparse
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
