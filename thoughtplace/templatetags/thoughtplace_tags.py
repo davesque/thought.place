99fcadb18d1f9a3059b18766743b4c2df42d0e8f
David Sanders <davesque@gmail.com>
2018-03-24 14:21:09 -0600

    Add and use remove_tags filter

--- thoughtplace/templatetags/thoughtplace_tags.py
+++ thoughtplace/templatetags/thoughtplace_tags.py
@@ -1,4 +1,7 @@
+from hashlib import md5
+
 from lxml import html
+from lxml.html.clean import Cleaner
 
 from django import template
 from django.utils.safestring import mark_safe
@@ -24,5 +27,22 @@ def add_classes(field, class_name):
 
     existing_classes = tree.attrib['class']
     tree.attrib['class'] = f'{existing_classes} {class_name}'
+
+    return mark_safe(html.tostring(tree).decode('utf-8'))
+
+
+@register.filter
+def remove_tags(html_str, tags):
+    content_hash = md5(html_str.encode('utf-8')).hexdigest()
+    wrapper_class = f'remove-tags-wrapper-{content_hash}'
+
+    html_str = f'<div class="{wrapper_class}">{html_str}</div>'
+    tree = html.document_fromstring(html_str)
+
+    cleaner = Cleaner()
+    cleaner.kill_tags = tags.split()
+
+    tree = cleaner.clean_html(tree)
+    tree = tree.find_class(wrapper_class)[0]
 
     return mark_safe(html.tostring(tree).decode('utf-8'))
